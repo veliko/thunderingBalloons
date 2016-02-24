dc8112fa0f27062d56018053cc27d5c780300ae0
mjimaz <m.aljimaz@gmail.com>
2016-02-23 18:44:55 -0800

    use callback to setStates of places

--- client/lib/searchPlaces.js
+++ client/lib/searchPlaces.js
@@ -1,13 +1,14 @@
-var searchPlaces = (options, callback) => {
+var searchPlaces = function (options, callback) {
   var term = $('#term').val();
   var address = $('#address').val();
+
+  console.log('callback', callback);
 
   codeAddress(address, function(lat, lng){
     var query = {lat:lat, lng:lng, term:term};
     $.get('http://localhost:8080/places', query)
       .done(function (data){
-        console.log(data);
-        callback(data);
+        callback({placesList: data.businesses});
     }).fail(function (error){
       console.error('Yelp: Failed to receive places!', error);
     });
